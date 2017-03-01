50b9be7da73826a67479caa7c6320f09e19aa13e
bitgrind <keithstedman@gmail.com>
2017-03-01 11:50:54 -0800

    added input id and value and form id

---
+++ build.php
@@ -0,0 +1,56 @@
+<?php
+  print_r($_POST);
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Blossom Build</title>
+  </head>
+  <body>
+    <main>
+      <form action="?" method="post">
+        <div>
+          <h1>Intro</h1>
+          <label for="site-title">Site Title</label>
+          <input id="site-title" name="site-title">
+          <p>Site Title - Short Discription</p>
+        </div>
+        <div>
+          <h1>About</h1>
+          <div>
+            <h2>Who We Are:</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div>
+            <h2>What We Do:</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+        <div>
+          <h1>Contact</h1>
+          <div>
+            <h2>Contact - Name</h2>
+            <h2>Contact - Number</h2>
+            <h2>Contact - Email</h2>
+            <h2>Contact - Location</h2>
+          </div>
+          <div>
+            <h1>Social Media</h1>
+            <h2>Facebook</h2>
+            <h2>Instagram</h2>
+            <h2>Linkedin</h2>
+            <h2>twiiter</h2>
+          </div>
+        </div>
+        <div>
+          <h1>Img Header 1200px</h1>
+        </div>
+        <div>
+          <h1>Img Logo 400px</h1>
+        </div>
+        <input type="submit" type="text">
+      </form>
+    </main>
+  </body>
+</html>
