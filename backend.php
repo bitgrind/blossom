b14ca64137265133995bcf6b208f604f85ab69e4
bitgrind <keithstedman@gmail.com>
2017-03-06 10:25:54 -0800

    adding backend

---
+++ backend.php
@@ -0,0 +1,22 @@
+<?php
+
+  include_once "php/db-config.php";
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+    <footer></footer>
+  </body>
+</html>
