2da1517318ac4efcbd3c505f8b42c6e8b21ba2ff
Alex Lee <doomcat9167@gmail.com>
2017-03-06 16:57:01 -0800

    typed js repeats now

--- js/home-script.js
+++ js/home-script.js
@@ -3,19 +3,14 @@ $(function(){
   $("#js-typed").typed({
       // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
       stringsElement: $('#typed-strings'),
-      typeSpeed: 150,
+      typeSpeed: 200,
       backDelay: 500,
-      loop: false,
+      loop: true,
       contentType: 'html', // or text
       // defaults to false for infinite loop
-      loopCount: false,
-      callback: function(){ foo(); },
-      resetCallback: function() { newTyped(); }
+      loopCount: true,
   });
 
-  $(".reset").click(function(){
-      $("#typed").typed('reset');
-  });
   //REGISTRATION FORM
   $("#register").submit(function(){
     event.preventDefault();
