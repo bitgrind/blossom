2a43fdb2c79d5f95076b0aad487c69e545186b39
bitgrind <keithstedman@gmail.com>
2017-03-07 11:18:12 -0800

    make db connection

--- backend.php
+++ backend.php
@@ -32,7 +32,7 @@
   $contactZip                 = $_POST['contact-zip'];
 
 //USER VARIBLES
-  $userName ="";
+  //$userName ="";
 
 
 
@@ -48,37 +48,11 @@
     echo "connected!";
   }
 
-  $sql = 'SELECT * FROM users';
+  $sql = "SELECT * FROM users";
 
   $rs = $conn->query($sql);
 
-  if($rs === false) {
-    trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
-  } else {
-    $rows_returned = $rs->num_rows;
-  }
-
-  $rs->data_seek(0);
-  while($row = $rs->fetch_assoc()){
-      echo $row['col1'] . '<br>';
-  }
-
-
-  $rs->data_seek(0);
-  while($row = $rs->fetch_row()){
-      echo $row[0] . '<br>';
-  }
-
-  $rs=$conn->query($sql);
-
-  if($rs === false) {
-    trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
-  } else {
-    $arr = $rs->fetch_all(MYSQLI_ASSOC);
-  }
-  foreach($arr as $row) {
-    echo $row['co1'];
-  }
+  print_r($rs);
 
   //print_r("connection: " . $connection);
 
