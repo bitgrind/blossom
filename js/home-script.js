31b0b6ff15805333a69922cde2a6feb041e17113
Alex Lee <doomcat9167@gmail.com>
2017-03-06 16:52:17 -0800

    add typed js to the main title

--- js/home-script.js
+++ js/home-script.js
@@ -1,13 +1,29 @@
 $(function(){
 
+  $("#js-typed").typed({
+      // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
+      stringsElement: $('#typed-strings'),
+      typeSpeed: 150,
+      backDelay: 500,
+      loop: false,
+      contentType: 'html', // or text
+      // defaults to false for infinite loop
+      loopCount: false,
+      callback: function(){ foo(); },
+      resetCallback: function() { newTyped(); }
+  });
+
+  $(".reset").click(function(){
+      $("#typed").typed('reset');
+  });
   //REGISTRATION FORM
   $("#register").submit(function(){
     event.preventDefault();
-    location.href="build.html";
+    location.href= "build.html";
   });
   //LOGIN FORM
   $("#formLogIn").submit(function(){
     event.preventDefault();
-    location.href="admin.html";
+    location.href= "admin.html";
   });
 });
