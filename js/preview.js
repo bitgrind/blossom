39f6c3c15509d878e354d55ffdf73807cfbfb86a
Alex Lee <doomcat9167@gmail.com>
2017-03-08 14:15:44 -0800

    add php variables in preview.html

--- js/preview.js
+++ js/preview.js
@@ -1,22 +1,7 @@
 
-//php vars
-var test ="testString";
-var previewId = <?php echo $previewId;?>;
-var userId = <?php echo $userId; ?>;
-var contactId = <?php echo $contactId; ?>;
-var cssLink = <?php echo $cssLink; ?>;
-var businessName = <?php echo $businessName; ?>;
-var diffStatement = <?php echo $diffStatement; ?>;
-var description = <?php echo $description; ?>;
-var description2 = <?php echo $description2; ?>;
-var businessPitch = <?php echo $businessPitch; ?>;
-var service1 = <?php echo $service1; ?>;
-var facebook = <?php echo $facebook; ?>;
-var instagram = <?php echo $instagram; ?>;
-var linkedin = <?php echo $linkedin; ?>;
-var twitter = <?php echo $twitter; ?>;
+
 
 
 $(function(){
-  $(".intro-message h1").text("test");
+  $(".intro-message h1").text(previewId);
 });//jQuery
