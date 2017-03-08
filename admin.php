f8303b03ced4f098c31fd28eef73a03a692a832e
Kaya Winn <mail@psyreal.net>
2017-03-08 14:43:25 -0800

    add php variable to nav

--- admin.php
+++ admin.php
@@ -61,10 +61,9 @@
         <div class="col-md-6">
           <div class="logo">
             <h2><a href="index.html">Blossom</a></h2>
+            <h2><a href="index.html"><?php echo $businessName;?></a></h2>
+
               </div>
-        </div>
-        <div>
-          <h2><?php echo $businessName;?></h2>
         </div>
         <div class="col-md-6">
           <ul class="mainNav">
