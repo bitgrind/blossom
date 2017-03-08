206f6bc1d59a22f16ab641da9f109235a3444dd6
Alex Lee <doomcat9167@gmail.com>
2017-03-08 15:11:39 -0800

    ad

--- preview.php
+++ preview.php
@@ -56,7 +56,6 @@ $previewSql="SELECT * FROM 'content' WHERE 'contentId'=".$adminContentId;
      <!-- Add custom CSS here -->
      <link rel="stylesheet" href="">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-     <script src="js/preview.js"></script>
      <title>Preview your page</title>
    </head>
    <body>
