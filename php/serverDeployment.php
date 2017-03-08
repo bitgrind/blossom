7e6024799d63c627b75893cdf553403f45590ca0
Keith Stedman <keithstedman@gmail.com>
2017-03-08 11:26:02 -0800

    got rid of html in auto deploy

---
+++ php/serverDeployment.php
@@ -0,0 +1,21 @@
+<?php
+	$commands = array(
+		'echo $PWD',
+		'whoami',
+		'git pull',
+		'git status',
+		'git submodule sync',
+		'git submodule update',
+		'git submodule status',
+	);
+	// Run the commands for output
+	$output = '';
+	foreach($commands AS $command){
+		// Run it
+		$tmp = shell_exec($command);
+		// Output
+		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+		$output .= htmlentities(trim($tmp)) . "\n";
+	}
+	// Make it pretty for manual user access (and why not?)
+?>
