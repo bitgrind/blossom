c2a1ed3aedd76affdd5bd68a9c405ba473e62723
Alex Lee <doomcat9167@gmail.com>
2017-03-08 14:18:41 -0800

    change preview.php

--- preview.php
+++ preview.php
@@ -45,20 +45,8 @@ $previewSql="SELECT * FROM 'content' WHERE 'contentId'=".$previewContentId;
  <script type="text/javascript">
  //php vars
  var test ="testString";
- var previewId = <?php echo $previewId;?>;
- // var userId = <?php echo $userId; ?>;
- // var contactId = <?php echo $contactId; ?>;
- // var cssLink = <?php echo $cssLink; ?>;
- // var businessName = <?php echo $businessName; ?>;
- // var diffStatement = <?php echo $diffStatement; ?>;
- // var description = <?php echo $description; ?>;
- // var description2 = <?php echo $description2; ?>;
- // var businessPitch = <?php echo $businessPitch; ?>;
- // var service1 = <?php echo $service1; ?>;
- // var facebook = <?php echo $facebook; ?>;
- // var instagram = <?php echo $instagram; ?>;
- // var linkedin = <?php echo $linkedin; ?>;
- // var twitter = <?php echo $twitter; ?>;
+ var previewId =  <?php echo $previewId;?>;
+
 
  </script>
 
