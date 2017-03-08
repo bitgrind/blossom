79dd86cb90da9ddfa8ea77caf398096a67503ad6
Kaya Winn <mail@psyreal.net>
2017-03-08 14:36:31 -0800

    adds php variable to Admin

--- admin.php
+++ admin.php
@@ -61,11 +61,10 @@
         <div class="col-md-6">
           <div class="logo">
             <h2><a href="index.html">Blossom</a></h2>
+            <h3><?php echo $businessName;?></h3>
               </div>
         </div>
         <div>
-          <h2><?php echo $businessName;?></h2>
-        </div>
         <div class="col-md-6">
           <ul class="mainNav">
             <li>nav1</li>
