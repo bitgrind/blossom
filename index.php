a982b24e2ffd7852a2acba071a96acf44a9ce424
Keith Stedman <keithstedman@gmail.com>
2017-03-08 11:28:36 -0800

    added shell-exec

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include_once  "php/connect.php";
      header('Location: build.php?userId='.$newId);
    }
  }
- echo"new page input on index changeINDEX444444";
+ echo"new page input on index changeINDEX555555";
 ?>
 <!DOCTYPE html>
 <html>
