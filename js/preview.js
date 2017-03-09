b810512e0e7018ff8f6120425abfd71c48cd7be2
Alex Lee <doomcat9167@gmail.com>
2017-03-09 10:48:56 -0800

    add js for preview

--- js/preview.js
+++ js/preview.js
@@ -3,8 +3,15 @@
 
 
 $(function(){
-
-  $("#button").click(function(){
-
+  $("#change").submit(function(){
+    var e = document.getElementById("footerStyle");
+    var val = e.options[e.selectedIndex].value;
+    if (val === "st1"){
+      window.open("preview.php")
+    }else if (val ==="st2"){
+      window.open("demo2b.php")
+    }else if(val ==="st3"){
+      window.open("demo2.php")
+    }
   });
 });//jQuery
