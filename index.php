e2caf2df3787cc806365b3c4cee8fff09b37066b
Kaya Winn <mail@psyreal.net>
2017-03-08 17:50:19 -0800

    add space above section three heading

--- index.php
+++ index.php
@@ -94,12 +94,12 @@ include_once  "php/connect.php";
     <h1>Demo pages</h1>
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-    <li data-target="#myCarousel" data-slide-to="3"></li>
-  </ol>
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+      </ol>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox" id="caroImg">
@@ -114,24 +114,25 @@ include_once  "php/connect.php";
     </div>
 
       <div class="item">
-      <img src="img/temp4.jpg" alt="Flower">
+      <img src="img/temp4.jpg" alt="image four">
     </div>
-  </div>
+  </div><!-- carousel-inner -->
 
   <!-- Left and right controls -->
   <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
     <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
 </div>
-  </section>
+</section>
 
-  <section class="container" id="page3">
+<section class="container" id="page3">
     <div class="row">
+      <br />
       <h1>Here's how it works</h1>
       <p>
         After signing up users will be able to create and modify web pages and be able preview pages before publishing. At this time there is no automatic archiving of previous designs or configurations. Uploads are permitted but limited to specific image file
