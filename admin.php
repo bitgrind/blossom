723c9cb0f03e6d99aeb7db939661b2cc96c65513
Ryan <ryanjonesirl@gmail.com>
2017-03-08 16:43:46 -0800

    fixing color

--- admin.php
+++ admin.php
@@ -64,8 +64,8 @@
 
         <div class="col-md-5">
           <ul class="mainNav">
-            <li>Admin <a href="admin.php"></a></li>
-            <li>Domain<a href="https://www.godaddy.com/domains/domain-name-search"></a></li>
+            <li><a href="admin.php">Admin</a></li>
+            <li><a href="https://www.godaddy.com/domains/domain-name-search">Domain</a></li>
             <li>Support</li>
           </ul>
         </div>
