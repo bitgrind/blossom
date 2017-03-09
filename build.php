a64597947a3a7ca37bcd3e98d0ec302c9197851b
Kaya Winn <mail@psyreal.net>
2017-03-09 14:16:36 -0800

    update build.php

--- build.php
+++ build.php
@@ -1,8 +1,6 @@
 <?php
 include "php/connect.php";
 
-$contentUserId = $_GET["userId"];
-
 $contentBusinessName        = $_POST['business-name'];
 $contentDifferentStatement  = $_POST['different-statement'];
 $contentShortDesc           = $_POST['small-description'];
@@ -18,7 +16,6 @@ $contentLk                  = $_POST['social-link-linkedin'];
 $contentTw                  = $_POST['social-link-twitter'];
 
 $contactFullName            = $_POST['contact-full-name'];
-$contactContentId           = $contentUserId;
 $contactPhone               = $_POST['contact-phone'];
 $contactEmail               = $_POST['contact-email'];
 $contactAddress             = $_POST['contact-address'];
@@ -26,15 +23,11 @@ $contactCity                = $_POST['contact-city'];
 $contactState               = $_POST['contact-state'];
 $contactZip                 = $_POST['contact-zip'];
 
+$contentUserId = $_GET["userId"];
+$insertContent = "INSERT INTO `content`(`contentUserId`, `contentContactId`, `contentStyle`, `contentBusinessName`, `contentDifferentStatement`, `contentShortDesc`, `contentUniqueDesc`, `contentBusinessPitch`, `contentServices`, `contentFb`, `contentIg`, `contentLk`, `contentTw`) VALUES ('".$contentUserId."', '".$contentContactId."', '".$contentStyleUrl."', '".$contentBusinessName."', '".$contentDifferentStatement."', '".$contentShortDesc."', '".$contentUniqueDesc."', '".$contentBusinessPitch."', '".$contentService1."', '".$contentFb."', '".$contentIg."', '".$contentLk."', '".$contentTw."')";
+
 if($_POST['buildForm']) {
-  if($contactFullName) {
-    $insertContact = "INSERT INTO `contact`(`contactName`, `contactNumber`, `contactEmail`, `contactAddress`, `contactCity`, `contactState`, `contactZip`) VALUES ('".$contactFullName."', '".$contactPhone."', '".$contactEmail."', '".$contactAddress."', '".$contactCity."', '".$contactState."', '".$contactZip."')";
-    $result = $conn->query($insertContact);
-    $newContactId = mysqli_insert_id($conn);
-  }
-  if($contentBusinessName){
-    $insertContent = "INSERT INTO `content`(`contentUserId`, `contentContactId`, `contentStyle`, `contentBusinessName`, `contentDifferentStatement`, `contentShortDesc`, `contentUniqueDesc`, `contentBusinessPitch`, `contentServices`, `contentFb`, `contentIg`, `contentLk`, `contentTw`) VALUES ('".$contentUserId."', '".$newContactId."', '".$contentStyleUrl."', '".$contentBusinessName."', '".$contentDifferentStatement."', '".$contentShortDesc."', '".$contentUniqueDesc."', '".$contentBusinessPitch."', '".$contentService1."', '".$contentFb."', '".$contentIg."', '".$contentLk."', '".$contentTw."')";
-    $result2 = $conn->query($insertContent);
+  if($conn->query($insertContent)){
     $newContentId = mysqli_insert_id($conn);
     header('Location: admin.php?contentId='.$newContentId);
   }
@@ -54,67 +47,78 @@ $conn->close();
     <!-- Bootstrap -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!-- Custom CSS -->
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/stylesbuild.css" rel="stylesheet" type="text/css">
 </head>
-
 <body>
-    <div class="container">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h2>Questionnaire</h2>
-            </div>
-            <div class="panel-body">
-                <form name="buildForm" class="client-question-form" action="?" method="POST">
-                    <div class="col-md-12">
-                        <div class="input-group">
-                            <label for="business-name">What is your business name?</label>
-                            <input class="form-control" name="business-name" id="business-name" placeholder="Bob's Burgers" type="text" />
-                        </div>
-                        <div class="input-group">
-                            <label for="different-statement">What makes you different?</label>
-                            <input class="form-control" id="different-statement" name="different-statement" placeholder="We make the complex simple." type="text" />
-                        </div>
-                        <div class="input-group">
-                            <label for="small-description">Write a small description about what your company does.</label>
-                            <textarea class="form-control col-xs-12" rows="3" name="small-description" id="small-description" type="text"></textarea>
-                        </div>
-                        <div class="input-group">
-                            <label for="unique-statement">What makes your business unique?</label>
-                            <textarea class="form-control col-xs-12" rows="3" name="unique-statement" id="unique-statement" type="text"></textarea>
-                        </div>
-                        <div class="input-group">
-                            <label for="business-pitch">Write a small description, as if you're pitching your business to a new customer.</label>
-                            <textarea class="form-control col-xs-12" rows="3" name="business-pitch" id="business-pitch" type="text"></textarea>
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        <h3>Profile Details</h3>
-                        <div class="input-group">
-                            <span class="input-group-addon">Full Name      </span><input class="form-control" name="contact-full-name" type="text" id="contact-full-name" placeholder="Bob Belcher" />
-                        </div>
-                        <div class="input-group">
-                            <span class="input-group-addon">Phone Number</span><input class="form-control" name="contact-phone" id="contact-phone" type="text" placeholder="503-864-3213" />
-                        </div>
-                        <div class="input-group">
-                            <span class="input-group-addon">Email Address </span><input class="form-control" name="contact-email" id="contact-email" type="text" placeholder="example@mail.com" />
-                        </div>
-                        <div class="input-group">
-                            <span class="input-group-addon">Street Address</span><input class="form-control" name="contact-address" id="contact-address" type="text" placeholder="123 A Street" />
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-4 col-sm-4">
-                                <div class="input-group">
-                                    <span class="input-group-addon">City</span><input class="form-control" name="contact-state" id="contact-city" type="text" placeholder="City" />
+  <div class="container-fluid">
+    <div class="row header">
+      <div class="col-md-6">
+        <div class="logo">
+          <h2><a href="index.php">Blossom Build</a></h2>
+        </div><!-- logo -->
+      </div> <!-- col-md-6 -->
+      <div class="col-md-6">
+        <ul class="mainNav">
+          <li><a href="build.php">Build Questionnaire</a></li>
+          <li><a href="https://www.godaddy.com/domains/domain-name-search">Domain</a></li>
+          <li><a href="#">Support</a></li>
+          <li><a href="index.php">Log Out</a></li>
+        </ul>
+      </div><!-- close col-md-5 -->
+    </div><!-- row header close -->
+<div class="col-md-10">
+    <form name="buildForm" class="client-question-form" action="?" method="POST">
+    <div class="col-md-12">
+      <div class="input-group">
+        <label for="business-name">What is your business name?</label>
+        <input class="form-control" name="business-name" id="business-name" placeholder="Bob's Burgers" type="text" />
+    </div><!-- col-md-12 -->
+    <div class="input-group">
+      <label for="different-statement">What makes you different?</label>
+      <input class="form-control" id="different-statement" name="different-statement" placeholder="We make the complex simple." type="text" />
+    </div>
+    <div class="input-group">
+      <label for="small-description">Write a small description about what your company does.</label>
+      <textarea class="form-control col-xs-12" rows="3" name="small-description" id="small-description" type="text"></textarea>
+    </div>
+    <div class="input-group">
+      <label for="unique-statement">What makes your business unique?</label>
+      <textarea class="form-control col-xs-12" rows="3" name="unique-statement" id="unique-statement" type="text"></textarea>
+    </div>
+    <div class="input-group">
+      <label for="business-pitch">Write a small description, as if you're pitching your business to a new customer.</label>
+      <textarea class="form-control col-xs-12" rows="3" name="business-pitch" id="business-pitch" type="text"></textarea>
+  </div><!-- input group -->
+</div><!-- col-md-12 -->
+<div class="col-md-12">
+ <h3>Profile Details</h3>
+   <div class="input-group">
+     <span class="input-group-addon">Full Name </span><input class="form-control" name="contact-full-name" type="text" id="contact-full-name" placeholder="Bob Belcher" />
+   </div><!-- input group -->
+   <div class="input-group">
+     <span class="input-group-addon">Phone Number</span><input class="form-control" name="contact-phone" id="contact-phone" type="text" placeholder="503-864-3213" />
+  </div>
+  <div class="input-group">
+    <span class="input-group-addon">Email Address </span><input class="form-control" name="contact-email" id="contact-email" type="text" placeholder="example@mail.com" />
+  </div>
+  <div class="input-group">
+    <span class="input-group-addon">Street Address</span><input class="form-control" name="contact-streeaddress" id="contact-address" type="text" placeholder="123 A Street" />
+  </div>
+
+  <div class="row">
+    <div class="col-lg-4 col-sm-4">
+      <div class="input-group">
+        <span class="input-group-addon">City</span><input class="form-control" name="contact-state" id="contact-address" type="text" placeholder="City" />
+      </div>
+    </div>
+    <div class="col-lg-4 col-sm-4">
+      <div class="input-group">
+      <span class="input-group-addon">State</span><input class="form-control" name="contact-state" id="contact-address" type="text" placeholder="State" />
                                 </div>
                             </div>
                             <div class="col-lg-4 col-sm-4">
                                 <div class="input-group">
-                                    <span class="input-group-addon">State</span><input class="form-control" name="contact-state" id="contact-state" type="text" placeholder="State" />
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-sm-4">
-                                <div class="input-group">
-                                    <span class="input-group-addon">Zipcode</span><input class="form-control" name="contact-zip" id="contact-zip" type="text" placeholder="Zipcode" />
+                                    <span class="input-group-addon">Zipcode</span><input class="form-control" name="contact-state" id="contact-address" type="text" placeholder="Zipcode" />
                                 </div>
                             </div>
                         </div>
@@ -165,18 +169,18 @@ $conn->close();
                             />
                         </div>
                         <br />
+                        <div class="buttonStyles">
+                          <button name="buildForm" value="newBuildForm" class="btn btn-primary" type="submit" id="submitBtn">
+                          <span class="glyphicon glyphicon-list-alt iconStyle"></span>Save
+                        </button>
+                        </div> <!-- button styles -->
                     </div>
                     <!--closes col-md-6 -->
-                    <div class="buttonStyles">
-                        <button name="buildForm" value="newBuildForm" class="btn btn-primary" type="submit" id="submitBtn">
-                        <span class="glyphicon glyphicon-list-alt iconStyle"></span>Save
-                      </button>
-                    </div>
-              </div>
-            </form>
-        </div>
-    </div>
-    </div>
+
+                  </form> <!-- form builder -->
+                </div>
+  </div> <!-- container fluid -->
+
     <!-- JQuery CDN -->
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 </body>
