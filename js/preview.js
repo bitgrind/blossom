ebf49ecb2c2e380805bad11846a9396df9639257
Alex Lee <doomcat9167@gmail.com>
2017-03-09 11:03:09 -0800

    fix

--- js/preview.js
+++ js/preview.js
@@ -7,10 +7,10 @@ $(function(){
     var val = $("#styles").val();
     if (val === "st1"){
       window.open("preview.php", "_self")
-    }else if (val ==="st2", "_self"){
-      window.open("demo2b.php")
-    }else if(val ==="st3","_self"){
-      window.open("demo2.php")
+    }else if (val ==="st2"){
+      window.open("demo2b.php","_self")
+    }else if(val ==="st3"){
+      window.open("demo2.php","_self")
     }
   });
 });//jQuery
