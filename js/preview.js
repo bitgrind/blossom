9f2a841ef80d61bc3cb95b86893412bb8d1f8074
Alex Lee <doomcat9167@gmail.com>
2017-03-09 11:02:07 -0800

    add

--- js/preview.js
+++ js/preview.js
@@ -6,11 +6,11 @@ $(function(){
   $("#change").click(function(){
     var val = $("#styles").val();
     if (val === "st1"){
-      window.load("preview.php")
-    }else if (val ==="st2"){
-      window.load("demo2b.php")
-    }else if(val ==="st3"){
-      window.load("demo2.php")
+      window.open("preview.php", "_self")
+    }else if (val ==="st2", "_self"){
+      window.open("demo2b.php")
+    }else if(val ==="st3","_self"){
+      window.open("demo2.php")
     }
   });
 });//jQuery
