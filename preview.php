23ae80f557e802e202417e7315093bf1e5b99412
Keith Stedman <keithstedman@gmail.com>
2017-03-09 14:24:24 -0800

    changed preview html

--- preview.php
+++ preview.php
@@ -129,7 +129,7 @@ if($previewContactId){
     <!-- /.container -->
   </div>
   <!-- /.intro-header -->
-  <div class="content-section-a">
+  <div class="firstContent content-section-a">
     <div class="container">
       <div class="row">
         <div class="col-lg-5 col-sm-6">
@@ -214,30 +214,30 @@ if($previewContactId){
         </div>
       </div>
       <!-- /.container -->
-    </div>
-    <!-- /.banner -->
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <ul class="list-inline">
-              <li><a href="#home">Home</a>
-              </li>
-              <li class="footer-menu-divider">&sdot;</li>
-              <li><a href="#about">About</a>
-              </li>
-              <li class="footer-menu-divider">&sdot;</li>
-              <li><a href="#services">Services</a>
-              </li>
-              <li class="footer-menu-divider">&sdot;</li>
-              <li><a href="#contact">Contact</a>
-              </li>
-            </ul>
-            <p class="copyright text-muted small">Copyright &copy; Your Company 2013. All Rights Reserved</p>
+      <footer>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <ul class="list-inline">
+                <li><a href="#home">Home</a>
+                </li>
+                <li class="footer-menu-divider">&sdot;</li>
+                <li><a href="#about">About</a>
+                </li>
+                <li class="footer-menu-divider">&sdot;</li>
+                <li><a href="#services">Services</a>
+                </li>
+                <li class="footer-menu-divider">&sdot;</li>
+                <li><a href="#contact">Contact</a>
+                </li>
+              </ul>
+              <p class="copyright text-muted small">Copyright &copy; Your Company 2013. All Rights Reserved</p>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
+    <!-- /.banner -->
     <div id="footerStyle">
       <form>
         <div class="form-group">
