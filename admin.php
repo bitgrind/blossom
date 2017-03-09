fb7498079ff1640e3401f6facd8dab6ad368ae70
Ryan <ryanjonesirl@gmail.com>
2017-03-09 08:37:30 -0800

    cleaning code

--- admin.php
+++ admin.php
@@ -73,8 +73,8 @@
           <h5 id="sitName"><?php echo $adminBusinessName;?></h5>
         </div> <!-- col-md-1 -->
       </div>
-      <div class="row">
-          <div class="col-md-2 side-nav1">
+      <div class="row side-nav1">
+          <div class="col-md-2">
             <h2 class="dashboard-h2-style">Dashboard</h2>
             <div class="dropdown">
               <button class="btn btn-default dropdown-btn dropdown-toggle" type="button" id="menu1" data-toggle="dropdown"><span id="active-dropdown-text">Site 1</span>  <span class="caret"></span></button>
